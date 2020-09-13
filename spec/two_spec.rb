4ad54ffcb080772b1448e968684d4ae7189957f2
Charlie Tanksley <charlie.tanksley@gmail.com>
2020-09-13 09:44:05 -0400

    uses new IntcodeProgram
    This swaps in the IntcodeProgram for the old, clunky
    `Program`. Hopefully this will be easier to extend as needed.

--- spec/two_spec.rb
+++ spec/two_spec.rb
@@ -69,19 +69,19 @@ RSpec.describe Operation do
   end
 end
 
-RSpec.describe Program do
+RSpec.describe IntcodeProgram do
   context "regression tests" do
     {
       [1, 0, 0, 0, 99] => [2, 0, 0, 0, 99],
       [2, 3, 0, 3, 99] => [2, 3, 0, 6, 99],
       [2, 4, 4, 5, 99, 0] => [2, 4, 4, 5, 99, 9801],
       [1, 1, 1, 4, 99, 5, 6, 0, 99] => [30, 1, 1, 4, 2, 5, 6, 0, 99]
-    }.each_pair do |instructions, output|
-      it "converts #{instructions} to #{output}" do
-        program = Program.new(instructions)
-        program.run
+    }.each_pair do |instructions, expected_output|
+      it "converts #{instructions} to #{expected_output}" do
+        program = described_class.new(instructions)
+        output = program.run
 
-        expect(output).to eq(program.states)
+        expect(output).to eq(expected_output)
       end
     end
   end
