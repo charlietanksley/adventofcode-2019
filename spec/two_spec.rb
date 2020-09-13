a121b34371950df3643fbc4f12a4c8dc529fb130
Charlie Tanksley <charlie.tanksley@gmail.com>
2020-09-13 06:16:12 -0400

    consolodates part 1 tests into regression suite
    I know that what I have in part 1 isn't going to work going forward,
    but I also know that I don't want to break it. Since the content of
    the tests was good but the code itself was bare-bones and unhelpful,
    I'm opting to consolodate these into a single chunk of regression
    tests.

--- spec/two_spec.rb
+++ spec/two_spec.rb
@@ -3,38 +3,19 @@
 require_relative "../src/two"
 
 RSpec.describe Program do
-  subject { Program.new(instructions) }
+  context "regression tests" do
+    {
+      [1, 0, 0, 0, 99] => [2, 0, 0, 0, 99],
+      [2, 3, 0, 3, 99] => [2, 3, 0, 6, 99],
+      [2, 4, 4, 5, 99, 0] => [2, 4, 4, 5, 99, 9801],
+      [1, 1, 1, 4, 99, 5, 6, 0, 99] => [30, 1, 1, 4, 2, 5, 6, 0, 99]
+    }.each_pair do |instructions, output|
+      it "converts #{instructions} to #{output}" do
+        program = Program.new(instructions)
+        program.run
 
-  before do
-    subject.run
-  end
-
-  context "a simple addition program" do
-    let(:instructions) { [1, 0, 0, 0, 99] }
-
-    it "updates the state correctly" do
-      expect(subject.states).to eq([2, 0, 0, 0, 99])
-    end
-  end
-
-  context "a simple multiplication program" do
-    let(:instructions) { [2, 3, 0, 3, 99] }
-    it "updates the state correctly" do
-      expect(subject.states).to eq([2, 3, 0, 6, 99])
-    end
-  end
-
-  context "next" do
-    let(:instructions) { [2, 4, 4, 5, 99, 0] }
-    it "does" do
-      expect(subject.states).to eq([2, 4, 4, 5, 99, 9801])
-    end
-  end
-
-  context "again" do
-    let(:instructions) { [1, 1, 1, 4, 99, 5, 6, 0, 99] }
-    it "does" do
-      expect(subject.states).to eq([30, 1, 1, 4, 2, 5, 6, 0, 99])
+        expect(output).to eq(program.states)
+      end
     end
   end
 end
