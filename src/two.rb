95b7b47622dad701bd052bcbb637814498b59853
Charlie Tanksley <charlie.tanksley@gmail.com>
2020-09-13 09:44:05 -0400

    uses new IntcodeProgram
    This swaps in the IntcodeProgram for the old, clunky
    `Program`. Hopefully this will be easier to extend as needed.

--- src/two.rb
+++ src/two.rb
@@ -18,6 +18,10 @@ class Instructions
     @instructions[position] = value
   end
 
+  def to_a
+    instructions
+  end
+
   def size
     instructions.size
   end
@@ -74,84 +78,33 @@ class Operation
     instructions[opcode_position]
   end
 end
-# A program
-class Program
-  attr_reader :states
 
-  def initialize(states)
-    @states = states
-    @running = true
-    @position = 0
+# The main touchpoint for an Intcode Program. Takes an instruction set
+# (an array of integers) as input and returns an instruction set
+# (array of integers) as output.
+class IntcodeProgram
+  def initialize(instructions)
+    @instructions = Instructions.new(instructions)
   end
 
   def run
-    step while running?
-  end
+    position = 0
+    instructions.size.times do
+      operation = Operation.new(position, instructions)
 
-  def step
-    if value_at(position) == 99
-      @running = false
-    elsif value_at(position) == 1
-      ap = position + 1
-      bp = position + 2
-      np = position + 3
-      a = value_at(value_at(ap))
-      b = value_at(value_at(bp))
-      y = position
-      next_location = value_at(np)
-      states[next_location] = a + b
-      @position += 4
-    elsif value_at(position) == 2
-      ap = position + 1
-      bp = position + 2
-      np = position + 3
+      break if operation.terminate?
 
-      a = value_at(value_at(ap))
-      b = value_at(value_at(bp))
-      y = position
-      next_location = value_at(np)
-      states[next_location] = a * b
-      @position += 4
-    else
-      @position += 1
+      if operation.skip?
+        position += 1
+      else
+        instructions[operation.replacement_position] = operation.value
+        position += 4
+      end
     end
-  end
-
-  def sub(hsh)
-    hsh.each do |k, v|
-      states[k] = v
-    end
-  end
-
-  def value_at(index)
-    Integer(states[index])
+    instructions.to_a
   end
 
   private
 
-  attr_accessor :position, :running
-
-  def running?
-    running
-  end
-end
-
-def main(input = nil)
-  max = 0
-  (0..99).each do |one|
-    (0..99).each do |two|
-      program = Program.new(input)
-      subs = { 1 => one, 2 => two }
-      p [program.value_at(0), program.value_at(1), program.value_at(2)].join(" - ")
-      program.sub(subs)
-      begin
-        program.run
-        value = program.value_at(0)
-        max = value if value > max
-        return "Yes" if value == 19_690_720
-      rescue TypeError => e
-      end
-    end
-  end
-  p max
+  attr_reader :instructions
 end
